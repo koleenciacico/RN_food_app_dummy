5c8baba5a03a8d22312a4eaaf7be4fcff96d9b1a
keicee <152405688+koleenciacico@users.noreply.github.com>
2025-08-21 21:24:13 +0800

    trying sentry

--- app/(home_tabs)/_layout.tsx
+++ app/(home_tabs)/_layout.tsx
@@ -2,7 +2,7 @@ import { Redirect, Slot } from 'expo-router';
 
 export default function _layout() {
 
-  const isAuthenticated = false;
+  const isAuthenticated = true;
 
   if (!isAuthenticated) return <Redirect href= "/sign-in"/>
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,28 @@
-
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 import "./global.css";
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://d9a700e33ed5425aa96e82cad3996212@o4509756808298496.ingest.us.sentry.io/4509882184171520',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 
 
-export default function RootLayout() {
+export default Sentry.wrap(function RootLayout() {
   const [fontsLoaded, error] = useFonts({
     "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
     "QuickSand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
@@ -21,4 +37,4 @@ useEffect(() => {
 }, [fontsLoaded,error])
 
   return <Stack screenOptions={{headerShown: false}}/>;
-}
+});
